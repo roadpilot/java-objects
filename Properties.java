880ccf738e92a90ec1cbd53cf458e599618a9cb4
roadpilot <mdacht777@gmail.com>
2021-11-16 07:09:23 -0700

    definitions

--- Properties.java
+++ Properties.java
@@ -2,3 +2,14 @@ public class Properties {
     public static void main(String[] args){
     }      
 }
+
+/**
+Objects have properties
+- name
+- size
+- color
+Objects have behaviors
+- shop
+- buy
+- display
+ */
