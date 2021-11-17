636039cf30522e01a6600028cb8680f03de7d202
roadpilot <mdacht777@gmail.com>
2021-11-17 08:58:08 -0700

    convert sysout to return

--- Calculator.java
+++ Calculator.java
@@ -3,11 +3,19 @@ public class Calculator {
         System.out.println("Nothing to do.");
     }
 
-    public void addSelf(int x) {
-        System.out.println(x + x);
+    // public void addSelf(int x) {
+    //     System.out.println(x + x);
+    // }    
+
+    // public void subtract2(int x, double y) {
+    //     System.out.println(x-y);
+    // }
+
+    public int addSelf(int x) {
+        return x + x;
     }    
 
-    public void subtract2(int x, double y) {
-        System.out.println(x-y);
+    public double subtract2(int x, double y) {
+         return x - y;
     }
 }
--- MethodDemo.java
+++ MethodDemo.java
@@ -1,7 +1,9 @@
 public class MethodDemo {
     public static void main(String[] args) {
         Calculator calc = new Calculator();
-        calc.addSelf(2);
-        calc.subtract2(50,5);
+        int sum = calc.addSelf(2);
+        double diff = calc.subtract2(50,5);
+        System.out.println(sum);
+        System.out.println(diff);
     }
 }
