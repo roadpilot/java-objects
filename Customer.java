220e38adffbedbf2b1045e4ea2159fac6ca1d265
roadpilot <mdacht777@gmail.com>
2021-11-17 08:45:51 -0700

    test constructor

--- Customer.java
+++ Customer.java
@@ -1,5 +1,3 @@
-
-
 public class Customer {
     // Fields / properties
     public String name = "";
@@ -7,8 +5,15 @@ public class Customer {
     public String address;
     public int    orderNum;
     public int    age;
-    
 
+    // Constructor (implicit if not defined / declared)
+    public Customer(){
+        System.out.println("Constructor called!");
+        address = "--description needed--";
+        orderNum = 0;
+        age = 0;
+    }
+    
     // Methods / behaviors
     public void displayCustomer() {
         System.out.println("Name: " + name + ", Age: " + age);
