1e70fb7e78a5bd1a89233695409af9590f464b9a
roadpilot <mdacht777@gmail.com>
2021-11-17 08:51:56 -0700

    Method demo

---
+++ Calculator.java
@@ -0,0 +1,13 @@
+public class Calculator {
+    public void doNothing() {
+        System.out.println("Nothing to do.");
+    }
+
+    public void addSelf(int x) {
+        System.out.println(x + x);
+    }    
+
+    public void subtract2(int x, double y) {
+        System.out.println(x-y);
+    }
+}
---
+++ MethodDemo.java
@@ -0,0 +1,7 @@
+public class MethodDemo {
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        calc.addSelf(2);
+        calc.subtract2(50,5);
+    }
+}
